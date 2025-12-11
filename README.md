2f21d278a5f315af2533adf40b5c343e06c77d13
Hippo <hippolyte.salmon@cpe.fr>
2025-12-11 10:28:15 +0100

    Added STM32 CAN management code and alarm trigger

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ Repo du projet d'IoT de majeure électronique à CPE Lyon
 ## Branche Hippo
 
 * Code de calcul de la moyenne de l'écart de rapport cyclique
-* Gestion CAN (**TODO**)
-* Génération alarme (**TODO**)
+* Gestion CAN 
+* Génération alarme 
 
 ## Matériel
 
-Hard: STM32 Nucleo 144
+* Hard : STM32 Nucleo 144
+* Soft : STM32CubeIDE
 
--- main.c
+++ main.c
@@ -21,7 +21,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "stdbool.h"
+#include "math.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -65,6 +66,15 @@ static void MX_CAN1_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+CAN_TxHeaderTypeDef TxHeader;
+CAN_RxHeaderTypeDef RxHeader;
+
+uint8_t TxData[8];
+uint8_t RxData[8];
+
+uint32_t TxMailbox;
+
+
 bool integrateflag;
 float cyclic_ratio;
 uint32_t HighState;
@@ -110,6 +120,14 @@ int main(void)
   MX_CAN1_Init();
   /* USER CODE BEGIN 2 */
 
+  HAL_CAN_Start(&hcan1);
+
+  HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING);
+
+  TxHeader.DLC = 2 ; // Data length
+  TxHeader.IDE = CAN_ID_STD;
+  TxHeader.RTR = CAN_RTR_DATA;
+  TxHeader.StdId = 0x446; // ID
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -118,6 +136,11 @@ int main(void)
   {
 	  if(integrateflag){
 		  cyclic_ratio = areacalc();
+		  TxData[0] = cyclic_ratio;
+		  if (cyclic_ratio >= 0.6){
+			  TxData[1] = 0x1; //Alarm
+		  }
+		  HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox);
 	  }
     /* USER CODE END WHILE */
 
@@ -191,8 +214,8 @@ static void MX_CAN1_Init(void)
   hcan1.Init.Prescaler = 16;
   hcan1.Init.Mode = CAN_MODE_NORMAL;
   hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan1.Init.TimeSeg1 = CAN_BS1_1TQ;
-  hcan1.Init.TimeSeg2 = CAN_BS2_4TQ;
+  hcan1.Init.TimeSeg1 = CAN_BS1_3TQ;
+  hcan1.Init.TimeSeg2 = CAN_BS2_2TQ;
   hcan1.Init.TimeTriggeredMode = DISABLE;
   hcan1.Init.AutoBusOff = DISABLE;
   hcan1.Init.AutoWakeUp = DISABLE;
@@ -409,11 +432,19 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
 }
 
 float areacalc(void){
-	area = area + abs(mean_cr-cyclic_ratio - cyclic_ratio) - crbuff[crptr];
+	float current_error = fabsf(mean_cr - cyclic_ratio);
+
+	area = area + current_error - crbuff[crptr];
+	crbuff[crptr] = current_error;
 	crptr++;
-	crbuff[crptr] = cyclic_ratio;
+
+	if (crptr >= sizeof(crbuff)){
+		crptr = 0;
+	}
+
 	integrateflag = 0;
-	return cyclic_ratio;
+
+	return area;
 }
 
 /* USER CODE END 4 */
